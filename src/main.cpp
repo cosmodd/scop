d96737e08ab1c25c278abe61c3f3bc2977bfab40
Cosmo <lolesqua@gmail.com>
2024-07-20 16:53:27 +0200

    ✨ Shader compilation

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,68 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+std::string readFile(const char *path)
+{
+	std::string content;
+	std::ifstream file(path, std::ios::binary | std::ios::in | std::ios::ate);
+
+	if (!file.is_open())
+	{
+		std::cerr << "Failed to open file: " << path << std::endl;
+		return "";
+	}
+
+	content.resize(file.tellg());
+	file.seekg(0, std::ios::beg);
+	file.read(&content[0], content.size());
+	file.close();
+
+	return content;
+}
+
+GLuint compileShader(GLenum type, const char *source)
+{
+	GLuint id = glCreateShader(type);
+
+	glShaderSource(id, 1, &source, NULL);
+	glCompileShader(id);
+
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+
+	if (result == GL_FALSE)
+	{
+		int length = 1024;
+		char error[length];
+		glGetShaderInfoLog(id, length, &length, error);
+		std::cerr << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!" << std::endl;
+		std::cerr << error << std::endl;
+		glDeleteShader(id);
+		return 0;
+	}
+
+	return id;
+}
+
+GLuint createShader(const char *vertexShaderSource, const char *fragmentShaderSource) {
+	GLuint program = glCreateProgram();
+
+	GLuint vertexShader = compileShader(GL_VERTEX_SHADER, vertexShaderSource);
+	GLuint fragmentShader = compileShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
+
+	glAttachShader(program, vertexShader);
+	glAttachShader(program, fragmentShader);
+	glLinkProgram(program);
+
+	// Cleanup
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	return program;
+}
 
 void processInput(GLFWwindow *window)
 {
@@ -10,7 +72,23 @@ void processInput(GLFWwindow *window)
 
 int main(void)
 {
-	if (!glfwInit()) {
+	unsigned int VBO, VAO;
+	unsigned int shaderProgram;
+
+	float vertices[] = {
+		// first triangle
+		0.5f, 0.5f, 0.0f,	// top right
+		0.5f, -0.5f, 0.0f,	// bottom right
+		-0.5f, 0.5f, 0.0f,	// top left
+
+		// second triangle
+		0.5f, -0.5f, 0.0f,	// bottom right
+		-0.5f, -0.5f, 0.0f, // bottom left
+		-0.5f, 0.5f, 0.0f	// top left
+	};
+
+	if (!glfwInit())
+	{
 		std::cerr << "Failed to initialize GLFW" << std::endl;
 		return EXIT_FAILURE;
 	}
@@ -21,8 +99,9 @@ int main(void)
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Create Window
-	GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-	if (!window) {
+	GLFWwindow *window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
 		std::cerr << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return EXIT_FAILURE;
@@ -30,14 +109,40 @@ int main(void)
 	glfwMakeContextCurrent(window);
 
 	// Load GLAD
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
 		std::cerr << "Failed to initialize GLAD" << std::endl;
 		return EXIT_FAILURE;
 	}
 
+	shaderProgram = createShader(
+		readFile("./src/shaders/default.vs").c_str(),
+		readFile("./src/shaders/default.fs").c_str()
+	);
+
+	// VBO
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+	glEnableVertexAttribArray(0);
+
+	// VAO
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+	glEnableVertexAttribArray(0);
+
 	// Main Loop
-	while (!glfwWindowShouldClose(window)) {
+	while (!glfwWindowShouldClose(window))
+	{
 		processInput(window);
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
