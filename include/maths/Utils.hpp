e599d8686c8ef129361f5482e33fd9b1f8bd34c5
Cosmo <lolesqua@gmail.com>
2024-07-24 14:11:40 +0200

    Add clamp function to maths Utils

--- include/maths/Utils.hpp
+++ include/maths/Utils.hpp
@@ -5,4 +5,5 @@ namespace maths
 {
 	float radians(float degrees);
 	float degrees(float radians);
+	float clamp(float value, float min, float max);
 }
--- src/maths/Utils.cpp
+++ src/maths/Utils.cpp
@@ -11,4 +11,13 @@ namespace maths
 	{
 		return radians * 180.0f / M_PI;
 	}
+
+	float clamp(float value, float min, float max)
+	{
+		if (value < min)
+			return min;
+		if (value > max)
+			return max;
+		return value;
+	}
 }
